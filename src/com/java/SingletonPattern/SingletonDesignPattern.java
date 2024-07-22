034d054fd595fc24a9830f24f4c21b75f8629acf
Algoadmin <Algoadmin@DESKTOP-DPAO279>
2024-07-22 19:13:16 +0530

    Singleton Practice

--- src/com/java/SingletonPattern/SingletonDesignPattern.java
+++ src/com/java/SingletonPattern/SingletonDesignPattern.java
@@ -1,10 +1,38 @@
 package com.java.SingletonPattern;
 
+import java.awt.desktop.ScreenSleepEvent;
+
 public class SingletonDesignPattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Singleton.getInstance().doSingleton();
+
+		//Singleton.getInstance().doSingleton();
+		/*
+		 * Singleton obj = new Singleton(); Singleton obj2 = new Singleton();
+		 */
+		
+		/*
+		 * Singleton Obj1 = Singleton.getInstance(); Singleton Obj2 =
+		 * Singleton.getInstance();
+		 */
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				Singleton Obj1 =  Singleton.getInstance();
+			}
+		});
+		Thread t2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				Singleton Obj2 =  Singleton.getInstance();
+			}
+		});
+
+		t1.start();
+		t2.start();
 	}
 
 }
@@ -13,18 +41,23 @@ public class SingletonDesignPattern {
 class Singleton {
 	public static Singleton instance;
 
-	public static Singleton getInstance() {
+	public static synchronized Singleton getInstance() {
 		if(instance == null) {
-			instance = new Singleton();
+			synchronized (Singleton.class) {
+				if(instance == null) {
+					instance = new Singleton();
+				}
+			}
+			
 		}
 		return instance;
 	}
 	
 	public static void doSingleton() {
-		System.out.println("Do Singleton");
+		System.out.println("Do Singleton.");
 	}
 
-	public Singleton() {
+	private Singleton() {
 		System.out.println("Singleton Instantiated.");
 	}
 	
